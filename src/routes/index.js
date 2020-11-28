7abcc44ad651dcb02fbf29ed3dd601b4b0866b6d
Jay An <jayandn1999@gmail.com>
2020-11-29 04:10:30 +0700

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,15 +9,14 @@
 
 
 function route(app){
+    app.use('/.netlify/functions/server', homeRoute); 
     app.use('/event',eventRoute); // trang sự kiện
     app.use('/movie',movieRoute); // trang movie
     app.use('/account',accountRoute); // trang accout
     app.use('/movietime',movietimeRoute); // trang accout
     app.use('/ticket',ticketRoute); // trang accout
-
     app.use('/gift',giftRoute); // trang gift
     app.use('/theater',theaterRoute); // trang theater
     app.use('/',homeRoute); // trang chủ showw all phim
-  
 }
  module.exports = route;
