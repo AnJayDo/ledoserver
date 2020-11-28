2a1dfd220e91ae35a0cd29ca93f927c9b5d21fc5
Jay An <jayandn1999@gmail.com>
2020-11-29 03:31:51 +0700

    Update express

--- express/server.js
+++ express/server.js
@@ -1,22 +1,48 @@
 'use strict';
 const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
-const app = express();
-const bodyParser = require('body-parser');
+const path = require('path')
+const morgan = require('morgan');
+const exphbs  = require('express-handlebars')
+const serverless = require('serverless-http')
+var methodOverride = require('method-override')
+const db=require('./config/db');
+const { query } = require('express');
+const app = express()
+const route = require('./routes')
+var cors = require('cors')
+var validator = require('validator')
+require('dotenv').config()
+// connect db
+db.connect();
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+app.use(cors())
+app.use('/src/resoures',express.static(path.join(__dirname,'resoures'))) // hiển thị ảnh
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use(express.static(path.join(__dirname,'public')));
+// cài middleware cho phương thức post để nạp dữ liệu vào body, pt get thì đc tích hợp sẵn nên dữ liệu dc lưu sẵn vào query
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
+// để sử dụng method PUT
+app.use(methodOverride('_method'))
+
+
+
+app.use(morgan('combined'))
+//templte engine
+//định nghĩa các file  .handlebars có thể sữa lại hbs 
+// app.engine('handlebars', exphbs({
+//   extname:'handlebars'
+// }));
+// app.set('view engine', 'handlebars');
+// dẫn path tới file view
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views',path.join(__dirname,'view'))
+
+route(app); 
 
 module.exports = app;
 module.exports.handler = serverless(app);
